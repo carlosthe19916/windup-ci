38640974b84396ab5a5994f6d50ae1d235994d93
Carlos Feria <carlosthe19916@gmail.com>
2022-06-17 11:57:49 +0200

    Add utils

--- .github/scripts/set-version.sh
+++
@@ -1,5 +0,0 @@
-mvn -B versions:set versions:commit -DnewVersion="$NEXT_VERSION"
-git config --global user.email "carlosthe19916@gmail.com"
-git config --global user.name "Carlos Feria"
-git commit --allow-empty -a -m "🏁 Releasing version $NEXT_VERSION"
-git push origin HEAD:master
---
+++ release/utils.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+function setNextVersion() {
+    mvn -B versions:set versions:commit -DnewVersion="$NEXT_VERSION"
+    git config --global user.email "carlosthe19916@gmail.com"
+    git config --global user.name "Carlos Feria"
+    git commit --allow-empty -a -m "🏁 Releasing version $NEXT_VERSION"
+    # git push origin HEAD:master
+}
