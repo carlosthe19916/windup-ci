847e62ac76da0b1d4558cee2195dcd272573a1ef
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2022-06-17 15:21:45 +0200

    Allow process multiple modules

--- release/utils.sh
+++ release/utils.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function setNextVersion() {
-    mvn -B versions:set versions:commit -DnewVersion="$NEXT_VERSION"
+    mvn -B versions:set versions:commit -DnewVersion="$NEXT_VERSION" -DprocessAllModules
     git config --global user.email "carlosthe19916@gmail.com"
     git config --global user.name "Carlos Feria"
     git commit --allow-empty -a -m "🏁 Releasing version $NEXT_VERSION"
@@ -10,7 +10,7 @@ function setNextVersion() {
 
 function releaseVersion() {
     echo "Not releasing to Maven central yet"
-    
+
     # mvn -DskipTests \
     # release:prepare -DpreparationGoals='-DskipTests clean install' \
     # release:perform -P jboss-release,gpg-sign
