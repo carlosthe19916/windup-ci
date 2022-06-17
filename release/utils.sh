d01516f394a457bc74e39ec869d949900705df54
Carlos Feria <carlosthe19916@gmail.com>
2022-06-17 12:12:48 +0200

    Set next version

--- release/utils.sh
+++ release/utils.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function setNextVersion() {
-    # mvn -B versions:set versions:commit -DnewVersion="$NEXT_VERSION"
+    mvn -B versions:set versions:commit -DnewVersion="$NEXT_VERSION"
     git config --global user.email "carlosthe19916@gmail.com"
     git config --global user.name "Carlos Feria"
     git commit --allow-empty -a -m "🏁 Releasing version $NEXT_VERSION"
