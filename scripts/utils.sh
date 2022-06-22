814e2ceae7889dc00144c2c5dda1390cc46c08d0
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2022-06-22 10:45:49 +0200

    Use composite actions

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-function gitConfigGlobal() {
-    git config --global user.email "$GIT_USER_EMAIL"
-    git config --global user.name "$GIT_USER_NAME"
-}
+# function gitConfigGlobal() {
+#     git config --global user.email "$GIT_USER_EMAIL"
+#     git config --global user.name "$GIT_USER_NAME"
+# }
 
 function setReleaseVersion() {
     mvn -B versions:set versions:commit -DnewVersion="$NEXT_VERSION" -DprocessAllModules
-    gitConfigGlobal
-    git commit --allow-empty -a -m "🏁 Releasing version $NEXT_VERSION"
-    git push origin HEAD:master
+    # gitConfigGlobal
+    # git commit --allow-empty -a -m "🏁 Releasing version $NEXT_VERSION"
+    # git push origin HEAD:master
 }
 
 function release() {
@@ -22,7 +22,7 @@ function release() {
 
 function setNextDevelopmentVersion() {
     mvn -B versions:set versions:commit -DnewVersion="$NEXT_VERSION" -DprocessAllModules
-    gitConfigGlobal
-    git commit --allow-empty -a -m "⬆️  Next version $NEXT_VERSION"
-    git push origin HEAD:master
+    # gitConfigGlobal
+    # git commit --allow-empty -a -m "⬆️  Next version $NEXT_VERSION"
+    # git push origin HEAD:master
 }
