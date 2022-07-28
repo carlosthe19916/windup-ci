310f0392926f28d0f8847f03f9344c0fa0fbac21
carlosthe19916 <2582866+carlosthe19916@users.noreply.github.com>
2022-07-28 14:44:55 +0200

    change

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -7,7 +7,7 @@ function setReleaseVersion() {
 function release() {
     # mvn source:jar javadoc:jar verify gpg:sign install deploy \
 
-    mvn source:jar javadoc:jar verify gpg:sign install \
+    mvn package gpg:sign \
     -Dgpg.executable="$GPG_SCRIPT" -DskipTests \
     -s $SETTINGS_XML \
     -gs /home/runner/.m2/settings.xml
